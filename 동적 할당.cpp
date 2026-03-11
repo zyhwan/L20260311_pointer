ab5fec2600c74923b8f40803ff0ac73e6987534b
zyhwan <zyhwan010410@naver.com>
2026-03-11 17:47:54 +0900

    Update 동적 할당.cpp

--- 동적 할당.cpp
+++ 동적 할당.cpp
@@ -11,13 +11,20 @@ int main()
 
 	int* p = new int[Size];
 
+	if (!p)
+	{
+		return -1;
+	}
+
+
 	for (int i = 0; i < Size; ++i)
 	{
 		p[i] = i + 1;
 		cout << p[i] << " ";
 	}
 
+	//Heap에 있는 배열을 반환.
 	delete[] p;
-	p = nullptr;
+	p = nullptr; //이 포인터가 가리키는 곳이 없다는 의미.
 
 }
